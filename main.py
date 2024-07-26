d7e310644992e75d6e31d439e57a5f0caf2d9591
SeeJayEye <chris.j.iliff@gmail.com>
2024-07-25 22:37:34 -0400

    Final Commit for Bookstore Project

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #open and read Frankenstein
 import string
 def main():
-    charcount={}
     bookpath = "/home/cjiliff/workspace/github.com/CJ-Iliff/bookbot/books/frankenstein.txt"
     text = getbook(bookpath)
     word_count = countwords(text)
     charcount = countchars(text)
     print (f"There are {word_count} words in Frankenstein!")
-    print ("The character breakdown is as follows:")
-    print (charcount)
+    print ("---Begin Report of books/frankenstein.txt ---")
+    list_dict_charcount = dict_list(charcount)
+    sorted_charcount = characteraggregated(list_dict_charcount)
+    for key in sorted_charcount:
+        character = key["Character"]
+        number = key["Number"]
+        print (f"The {character} was found {number} times")
+    
+    
 
 def getbook(path):
     with open(path) as f:
@@ -33,5 +39,22 @@ def countchars(book):
             else:
                 char_dict[char] = 1
     return char_dict
-  
+
+def dict_list(matrix):
+    listed_matrix=[]
+    for char in matrix:
+        dict_row={}
+        dict_row["Character"] = char
+        dict_row["Number"] = matrix[char]
+        listed_matrix.append(dict_row)
+
+    return listed_matrix
+
+def characteraggregated(list):
+    list.sort(reverse=True, key=sort_on)
+    return list
+
+def sort_on(dict):
+    return dict["Number"]
+   
 main()
